311e131429f0aa76359151796fe4dc9ebf6a935e
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-06-07 12:56:06 +0530

    adding ec2 address

--- codearena-frontend/src/api/axiosInstance.jsx
+++ codearena-frontend/src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'http://13.60.15.110/api',//todo add ec2 public address
     headers: {
         'Content-Type': 'application/json',
     },
