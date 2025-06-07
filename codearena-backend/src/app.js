db45a901ea4dd98daf65f7d80320f5997f670bf0
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2025-06-07 16:47:14 +0530

    updating cors

--- codearena-backend/src/app.js
+++ codearena-backend/src/app.js
@@ -5,18 +5,21 @@ const app = express();
 const dbConnection = require("./config/db");
 const userRoutes = require("./routes/auth.routes");
 const problemRoute = require("./routes/problem.route");
-
 app.use(cors({
     origin: 'https://codearena-frontend.vercel.app',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 app.use(express.json());
 
 dbConnection();
 
 app.use('/api/auth', userRoutes);
 app.use('/api/problems', problemRoute);
+app.get('/test', (req, res) => {
+    res.json({msg: 'cors works'});
+});
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
